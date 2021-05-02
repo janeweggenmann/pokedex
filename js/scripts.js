1bb742d27a7162c373dbbcf7ddd4f78b72f96633
Jane Weggenmann <janeweggenmann@gmail.com>
2021-05-02 19:08:04 -0400

    Creating pokemonList variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
-alert('Hello world');
-
-let favoriteFood = "French Fries"
-document.write(favoriteFood)
+let pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    type: ["grass", "poison"]
+  },
+  {
+    name: "Butterfree",
+    height: 1.1,
+    type: ["bug", "flying"]
+  },
+  {
+    name: "Jigglypuff",
+    height: 0.5,
+    type: ["fairy", "normal"]
+  },
+  {
+    name: "Dewgong",
+    height: 1.7,
+    type: ["ice", "water"]
+  },
+  {
+    name: "Ledyba",
+    height: 1,
+    type: ["bug", "flying"]
+  },
+]
