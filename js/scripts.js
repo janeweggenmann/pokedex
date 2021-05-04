1f240c591dedc83d29ac943a63e0900b567545d0
Jane Weggenmann <janeweggenmann@gmail.com>
2021-05-03 22:59:19 -0400

    Add loop with conditional height

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,12 @@ let pokemonList = [
     type: ["bug", "flying"]
   },
 ]
+
+//list Pokemon name and height on page
+for (i=0; i<5; i++) {
+  if (pokemonList[i].height >1.5){
+    //if height over 1.5 then call out special large size
+    document.write (pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!" + "<br>");
+  }else
+    document.write (pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br>");
+}
