0967aec650e4becad792e67f0397223ef3b015d1
Jane Weggenmann <janeweggenmann@gmail.com>
2021-05-26 23:19:56 -0400

    Added notes

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,9 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=30';
+  //Selecting modal container id from HTML file
 
+  //add pokemon function
   function add(pokemon) {
     if (typeof pokemon === "object" &&
       "name" in pokemon)
@@ -17,6 +19,7 @@ let pokemonRepository = (function () {
     return pokemonList;
   }
 
+  //Add new list item and button for each pokemon
   function addListItem(pokemon) {
     let pokemonList = document.querySelector(".pokemon-list");
     let listItem = document.createElement("li");
@@ -28,13 +31,13 @@ let pokemonRepository = (function () {
     button.innerText = pokemon.name;
     button.classList.add("pokemon-button");
 
-    //When user clicks on pokemon, show details function will run
+    //When user clicks on pokemon, show details
     button.addEventListener('click', function() {
       showDetails(pokemon)
     });
   }
 
-//pull list of pokemon from API
+  //pull list of pokemon from API
   function loadList() {
     return fetch(apiUrl).then(function (response) {
       return response.json();
@@ -51,7 +54,7 @@ let pokemonRepository = (function () {
     })
   }
 
-//load details of pokemon from list pulled from API
+  //load details of pokemon from API
   function loadDetails(pokemon) {
     let url = pokemon.detailsUrl;
     return fetch(url).then(function (response) {
@@ -65,6 +68,17 @@ let pokemonRepository = (function () {
     });
   }
 
+  //Create a modal when "showModal" function is called
+    //When modal is created, also create a close button
+    //Hide the modal when the close button is clicked
+    //When modal is created, create h1 and p elements
+    //Append close button, h1, and p to modal
+    //Append modal to modal container
+    //When showModal function is called, make modal visible
+  //To hide the modal, take away the "is visible" class
+  //Hide the modal when the Escape key is pressed
+  //Hide the modal when you click outside of the modal
+  //When showing details, load details and show modal
   function showDetails(pokemon) {
     pokemonRepository.loadDetails(pokemon).then(function () {
       console.log(pokemon);
@@ -81,6 +95,7 @@ let pokemonRepository = (function () {
   };
 })();
 
+//
 pokemonRepository.loadList().then(function () {
   pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
