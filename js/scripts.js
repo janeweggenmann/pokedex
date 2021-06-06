3b7412543a270a615db5da877617e28d0f079f4c
Jane Weggenmann <janeweggenmann@gmail.com>
2021-06-05 20:51:39 -0400

    Update API limit

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //IIFE Function
 let pokemonRepository = (function() {
   let pokemonList = [];
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=30';
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=300';
 
   //add pokemon function
   function add(pokemon) {
